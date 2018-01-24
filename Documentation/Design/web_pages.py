653ced8b7ff63cbcec805735e6dd5e28304a96d1
ntdragon <birdsall_99@comcast.net>
2018-01-24 13:16:34 -0500

    WOrking with Mike on html pages

--- Documentation/Design/web_pages.py
+++ Documentation/Design/web_pages.py
@@ -2,52 +2,66 @@
 """
 Display web pages page for Wish List site.
 """
+class HtmlPage():
+     """
+     """
+     def __init__(self):
+          self.page = []
+          
 
-def display_web_header():
-     """
-     Display the start of any web page
-     """
-     print("""<!DOCTYPE html
-     PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-     "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-     <html xmlns="http://www.w3.org/1999/xhtml" lang="en-US" xml:lang="en-US>
-     """)
+     def display_header(self, page_title="HUH"):
+          """
+          Display the start of any web page
+          """
+          self.page.append("""<!DOCTYPE html
+          PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+          "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+          <html xmlns="http://www.w3.org/1999/xhtml" lang="en-US" xml:lang="en-US>
+          <head>
+          <title>{page_title}</title>
+          <link rel="stylesheet" type="text/css" href="/css/wls_default.css" />
+          <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+          </head>
+          """.format(page_title=page_title))
 
-def display_webpage_head(page_title):
-     """
-     Display the <head> of each webpage
-     """
-     print("""<head>
-     <title>""")
-     print(page_title)
-     print("""</title>
-     <link rel="stylesheet" type="text/css" href="/css/wls_default.css" />
-     <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-     </head>
-     """)
+     def display_body(self, body):
+          pass
 
-def display_webpage_footer():
-     """
-     Display the footer for all the web pages.
-     """
-     print("""</html>""")
+     def display_footer(self, footer):
+          """
+          Display the footer for all the web pages.
+          """
+          self.page.append("""</html>""")
 
-def display_admin_site_body():
-     """
-     Display the body of the admin site webpage
-     """
+     def print_page(self):
+          print("".join(self.page))
 
-def display_create_user_body():
+class AdminPage(HtmlPage):
+     def display_admin_site_body(self, page):
+          """
+          Display the body of the admin site webpage
+          """
+
+class HomePage(HtmlPage):
+     pass
+
+
+
+
+
+'''
+def display_create_user_body(page):
      """"
      Display the body of the create user webpage
      """
 
-def display_edit_user_body():
+def display_edit_user_body(page):
      """
      Display the body of the edit user webpage
      """
 
-def display_user_purchases_body():
+def display_user_purchases_body(page):
      """
      Display the body of the user purchases webpage
      """
+'''
