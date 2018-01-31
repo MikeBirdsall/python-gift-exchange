9d2776cdc5addb4d3d4e4e7922f86d68de1a483d
MikeBirdsall <mgbirdsall@gmail.com>
2018-01-31 02:41:42 -0500

    Sigificant additions to conversion and unit test

--- src/conversion/convertperson.py
+++ src/conversion/convertperson.py
@@ -9,6 +9,7 @@
 import argparse
 import sqlite3
 from os.path import splitext
+import os
 
 
 class person:
@@ -29,52 +30,82 @@ class person:
             self.email]
 
 
-def convert(files, db):
+class convert():
     "Do conversion across files "
 
-    # userid from filename
-    # fullname from name field
-    # nickname from nickname (later)
-    # birthday from birthday (later)
-    # email from email (later?)
+    def __init__(self, db=None):
+        self.files = []
+        if db:
+            self.connection = sqlite3.connect(db)
+        else:
+            self.connection = None
+
+    def reset_db(self, connection):
+        self.connection = connection
+
+    def run(self, files):
+        if not self.connection:
+            print("Warning: No database connected")
+            return
+
+        # userid from filename
+        # fullname from name field
+        # nickname from nickname (later)
+        # birthday from birthday (later)
+        # email from email (later?)
 
 
-    people = []
-    for personfile in files:
-        # Using one file per person, can we get the file name?
-        userid = splitext(personfile.name)[1][1:]
-        this_person = person(userid)
-
-        for line in personfile:
-            if line.startswith("#") or not line.strip():
+        people = []
+        for personfile in files:
+            # Using one file per person, can we get the file name?
+            if not os.stat(personfile.name).st_size:
+                print("Warning: Empty personfile {}".format(personfile.name))
                 continue
+            userid = splitext(personfile.name)[1][1:]
+            if not userid:
+                print("Warning: No userid found in {}".format(personfile.name))
+                continue
+            this_person = person(userid)
 
-            field, value = line.strip().split("=", 1)
-            if field == "name":
-                this_person.fullname = value
-            elif field == 'nickname':
-                this_person.nickname = value
-            elif field == 'birthday':
-                this_person.birthday = value
-            elif field == 'email':
-                this_person.email = value
-            elif field in ('group', 'exclude', 'spouse', 'admin'):
-                pass
-            else:
-                print("Unknown field %s in %s" % (field, personfile.name))
-        people.append(this_person.params())
+            for line in personfile:
+                if line.startswith("#") or not line.strip():
+                    continue
 
-    connection = sqlite3.connect(db)
-    cursor = connection.cursor()
-    try:
-        cursor.executemany("""insert into person
-            ('userid', 'fullname', 'nickname', 'birthday', 'email') VALUES
-            (?,?,?,?,?)""", people)
-    except sqlite3.IntegrityError as e:
-        print("Couldn't put %s in database: %s" % (userid, e))
+                field, value = line.strip().split("=", 1)
+                if field == "name":
+                    this_person.fullname = value
+                elif field == 'nickname':
+                    this_person.nickname = value
+                elif field == 'birthday':
+                    this_person.birthday = value
+                elif field == 'email':
+                    this_person.email = value
+                elif field in ('group', 'exclude', 'spouse', 'admin'):
+                    pass
+                else:
+                    print("Warning: Unknown field %s in %s" % (field, personfile.name))
+            people.append(this_person.params())
 
-    connection.commit()
-    connection.close()
+        if not people:
+            print("Warning: no people defined")
+            return
+
+        cursor = self.connection.cursor()
+        try:
+            # Debugging to print the tables
+            #cursor.execute("select name from sqlite_master where type='table'")
+            #print("tables:", cursor.fetchall())
+
+            cursor.executemany("""insert into person
+                ('userid', 'fullname', 'nickname', 'birthday', 'email') VALUES
+                (?,?,?,?,?)""", people)
+        except sqlite3.IntegrityError as e:
+            print("Warning: Couldn't put %s in database: %s" % (userid, e))
+
+        self.connection.commit()
+
+    def ___del__(self):
+        self.connection.close()
 
 
 def main():
@@ -87,7 +118,7 @@ def main():
         help="Input file[s]")
     args = parser.parse_args()
 
-    convert(args.infile, args.database)
+    convert(args.database).run(args.infile)
 
 if __name__ == "__main__":
     main()
