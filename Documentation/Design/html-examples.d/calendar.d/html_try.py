d731f0d98a0a783daa7b02fb40abcb53bc500b71
ntdragon <30935596+ntdragon@users.noreply.github.com>
2019-02-13 18:31:02 -0500

    Work on calendar and User Home page - needs work on variable amounts of events and wishes

--- Documentation/Design/html-examples.d/calendar.d/html_try.py
+++ Documentation/Design/html-examples.d/calendar.d/html_try.py
@@ -2,53 +2,47 @@
 
 from jinja2 import Environment, FileSystemLoader
 
-hd = {"loc": "Month Calendar"}
-hdr = { "name": "Ed",  "page":"March 2019 Calendar", "today":"Saturday  March  2, 2019" }
-pref = {  "startDay":1,  "startwk":5}
+hd = {"loc":"Month Calendar"}
+hdr = { "name":"Ed",  "page":"March 2019 Calendar", "today":"Saturday  March  2, 2019" }
+
 days = ('Sunday Monday Tuesday Wednesday Thursday Friday Saturday Sunday'.split())
-colorsm = {"previousmonth": "DarkOrchid", "thisbefore": "Aqua",  "today": "Yellow",  "thismonth": "White",  "nextmonth": "Lime", "site":"Red" }
-colorsc = { "neutral": "silver",  "calAclr": "green",  "calBclr": "blue",  "calCclr": "cyan",  "calDclr": "magenta",  "calEclr": "purple",  "calSclr": "red" }
-cal = {"month":"March", "year":"2019"}
+colorsm = {"priormonth": "DarkOrchid", "thisbefore": "Aqua",  "today": "Yellow",  "thismonth": "White",  "nextmonth": "Lime", "site":"Red" , "neutral": "silver", "calSclr": "red" }
+
+# devt is intended to hold the number of events for the day to be used in the template with an if or for but can't get to work right now
+tdy = []
+for i in range(0, 35,1):
+     tdy.append({"bgtclr":"white","bgeclr":"white", "dnum":0, "devt":-1, "devt1t":"", "devt1c":"",  "devt2t":"", "devt2c":"",  "devt3t":"", "devt3c":"",  "devt4t":"", "devt4c":"", })
+ 
+
+cal = {"month":"March", "year":"2019", "startwk":5, "calAt":"Liturgical", "calBt":"US Holidays","calCt":"Birdsall Family", "calDt":"Kirkup Family", "calEt":""}
+pref = {  "startDay":1,  "calAclr": "green",  "calBclr": "blue",  "calCclr": "cyan",  "calDclr": "magenta",  "calEclr": "purple"}
+dts = [25, 26, 27, 28, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31 ]
+
+for i in range(0, 4):
+          tdy[i]["bgtclr"] = colorsm["priormonth"]
+          tdy[i]["bgeclr"] = colorsm["priormonth"]
 
 
-bgtclr = [colorsm["thismonth"]]
-bgeclr = ["white"]
-dnum = [0]
-calat = [""]
-calbt = [""]
-calct = [""]
-caldt = [""]
-calet = [""]
-calst = [""]
-
-for i in range (1, 34,1):
-     bgtclr.append( colorsm["thismonth"])
-     bgeclr.append("white")
-     dnum.append(0)
-     calat.append("")
-     calbt.append("")
-     calct.append("")
-     caldt.append("")
-     calet.append("")
-     calst.append("")
-     
-dnum = [25, 26, 27, 28, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31 ]
-for i in range (0, 4):
-          bgtclr[i] = colorsm["previousmonth"]
-          bgeclr[i] = colorsm["previousmonth"]
-
-bgtclr[0] = colorsm["previousmonth"]
-bgtclr[4] = colorsm["thisbefore"]
-bgtclr[5] = colorsm["today"]
-bgeclr[4] = colorsm["thisbefore"]
-bgeclr[5] = colorsm["today"]
-calat[9] = "Ash Wednesday"
-calbt[13] = "DST begins"
-calbt[21] = "President's Day"
+for i in range(0,35,1):
+     tdy[i]["dnum"] = dts[i]
 
 
+tdy[4]["bgtclr"] = colorsm["thisbefore"]
+tdy[4]["beeclr"] = colorsm["thisbefore"]
+tdy[5]["bgtclr"] = colorsm["today"]
+tdy[5]["beeclr"] = colorsm["today"]
+tdy[9]["devt"] = 1
+tdy[9]["dev1t"] =  "Ash Wednesday"
+tdy[9]["dev1c"] =pref["calAclr"]
+tdy[13]["devt"] = 1
+tdy[13]["dev1t"] =  "DST begins"
+tdy[13]["dev1c"] = pref["calBclr"]
+tdy[21]["devt"] = 1
+tdy[21]["dev1t"] =  "President's Day"
+tdy[21]["dev1c"] = pref["calBclr"]
 
-input_ = {"hd":hd, "hdr":hdr, "cal":cal, "dnum":dnum, "colorsm":colorsm, "colorsc":colorsc, "bgtclr":bgtclr, "bgeclr":bgeclr, "pref":pref, "days":days, 'calat':calat, 'calbt':calbt, 'calct':calct, 'caldt':caldt,'calet':calet, 'calst':calst}
+
+input_ = {"hd":hd, "hdr":hdr, "cal":cal, "days":days, "colorsm":colorsm, "pref":pref, "tdy":tdy }
 env = Environment(loader = FileSystemLoader("."))
 template=env.get_template("calendar3.html")
 
--- Documentation/Design/html-examples.d/user_home.d/html_try.py
+++ Documentation/Design/html-examples.d/user_home.d/html_try.py
@@ -4,16 +4,69 @@ from jinja2 import Environment, FileSystemLoader
 
 hd = {"loc": "User Home"}
 hdr = { "name": "Ed",  "page":"Home", "today":"Wednesday  March 06, 2019" }
+
 user = {"firstn":"Edward", "lastn":"Birdsall", "bday":"23", "byr":"1952", "bmn":"April", "pemail":"birdsall_99@comcast.net", "semail":"ntdgn@umich.edu", "clan":"Birdsall" }
 msgs = {"new":0, "tot":10}
 ss = {"cyr":2018, "cypk":"Peggy Kovas" }
+evt = {"event1": "None", "bday1":"Mar 17 - Terri McIntosh", "bday2":"Mar 29 - Alexa Hodgins"}
+mod = {"who":"owner Edward Birdsall", "date":"Sunday Fed 24, 2019  13:15:12"}
+
+wsh = []
+for i in range(0, 16, 1):
+     wsh.append({"num":i, "ndes":"1","des":"","exp":""})
+
+wsh[1]["ndes"] = "any"
+wsh[1]["des"] = "Frangelico, B&B, Irish Mist"
+wsh[1]["exp"] = "never"
+wsh[2]["ndes"] = "any"
+wsh[2]["des"] = "Fudge - chocolate, peanut butter are favorites"
+wsh[2]["exp"] = "never"
+wsh[3]["ndes"] = "any"
+wsh[3]["des"] = "Wine - Favorites yellowtail-*, wallaby creek-*, and others"
+wsh[3]["exp"] = "never"
+wsh[4]["ndes"] = "any"
+wsh[4]["des"] = "Gift Certificate - Home Depot or Lowes"
+wsh[4]["exp"] = "never"
+wsh[5]["ndes"] = "any"
+wsh[5]["des"] = "Gift Certificate - Barnes and Noble"
+wsh[5]["exp"] = "never"
+wsh[6]["ndes"] = "any"
+wsh[6]["des"] = "Gift Certificate - Right Stuf (www.rightstuf.com) Where I get most of my anime and shaw dvds"
+wsh[6]["exp"] = "never"
+wsh[7]["ndes"] = "1"
+wsh[7]["des"] = "Rasberry Pi Starter/learning kit"
+wsh[7]["exp"] = "Dec 26, 2019"
+wsh[8]["ndes"] = "any"
+wsh[8]["des"] = "Candle molds especially for the larger candles (larger than votive)"
+wsh[8]["exp"] = "never"
+wsh[9]["ndes"] = "any"
+wsh[9]["des"] = "washable long ties for church (not dryclean) looking for ties with primary color rose, white."
+wsh[9]["exp"] = "Dec 26, 2019"
+wsh[10]["ndes"] = "1"
+wsh[10]["des"] = "CD by The Fire - Inspired (found at store.cdbaby.com)"
+wsh[10]["exp"] = "Dec 26, 2019"
+wsh[11]["ndes"] ="any"
+wsh[11]["des"] = "Short sleeve Shirts 16 neck in red, rose, green, violet - liturgical colors to wear to church (same as ties)"
+wsh[11]["exp"] = "Dec 26, 2020"
+wsh[12]["ndes"] = "any"
+wsh[12]["des"] = "Washable Ties in red, rose, green, violet - liturgical colors to wear to church"
+wsh[12]["exp"] = "Dec 26, 2020"
+wsh[13]["ndes"] = "1"
+wsh[13]["des"] = "Wishlist in final test mode"
+wsh[13]["exp"] = "Sep 01, 2019"
+wsh[14]["ndes"] = "1"
+wsh[14]["des"] = "2450 Canyon Sold"
+wsh[14]["exp"] = "Aug 01, 2019"
+wsh[15]["ndes"] = "1"
+wsh[15]["des"] = "New House in Gerogia"
+wsh[15]["exp"] = "Dec 31, 2020"
 
 
 
 
 
 
-input_ = {"hd":hd, "hdr":hdr, "user":user, "msgs":msgs , "ss":ss }
+input_ = {"hd":hd, "hdr":hdr, "user":user, "msgs":msgs , "ss":ss, "evt":evt, "mod":mod, "wsh":wsh }
 
 env = Environment(loader = FileSystemLoader("."))
 template=env.get_template("UserHome.html")
