85128ff67d01b5a88fd5f9c62e35af5a2bb77f70
ntdragon <30935596+ntdragon@users.noreply.github.com>
2019-07-14 09:59:32 -0400

    User shopping list work

--- Documentation/Design/html-examples.d/user_shop.d/html_try.py
+++ Documentation/Design/html-examples.d/user_shop.d/html_try.py
@@ -11,29 +11,30 @@ pers = dict(clan="Birdsall", yr="2018", sspk="Robert Birdsall", event="Christmas
 
 
 wsh = [
-    dict(num=1, pfor="Amy Kovas", npur=1, ndes="1", notes="",
+    dict(num=1, pfor="Amy Kovas", npur=1, nres="0", ndes="1", notes="",
          des="Psych: Season 4 (TV Series)"),
-    dict(num=2, pfor="Benjamin Birdsall", npur=1, ndes="any", notes="set #10232",
+    dict(num=2, pfor="Benjamin Birdsall", npur=1, nres="0", ndes="any", notes="set #10232",
          des="legos"),
-    dict(num=3, pfor="Robert Birdsall", npur=1, ndes="any", notes="Home Depot",
+    dict(num=3, pfor="Robert Birdsall", npur=1,  nres="0",ndes="any", notes="Home Depot",
          des="Gift card for Lowe's or Home Depot"),
-    dict(num=4, pfor="Robert Birdsall", npur=1, ndes="any", notes="Lowe",
+    dict(num=4, pfor="Robert Birdsall", npur=1,  nres="0",ndes="any", notes="Lowe",
          des="Gift Certificate - Home Depot or Lowes"),
-    dict(num=5, pfor="Bryan Kovas", npur=1, ndes="1", notes="",
+    dict(num=5, pfor="Bryan Kovas", npur=1,  nres="0",ndes="1", notes="",
          des="CD - Linkin Park: A Thousand Suns"),
-    dict(num=6, pfor="Derek Birdsall", npur=2, ndes="any", notes="",
+    dict(num=6, pfor="Derek Birdsall", npur=2,  nres="0",ndes="any", notes="",
          des="Hot Wheels or Matchbox Cars"),
-    dict(num=7, pfor="Erin Birdsall", npur=1, ndes="1",
+    dict(num=7, pfor="Erin Birdsall", npur=1,  nres="0",ndes="1",
          des="Celtic Woman or Celtic Thunder Cds", notes="Celtic Thundar"),
-    dict(num=8, pfor="Greg Kovas", npur=1, ndes="1", notes="",
+    dict(num=8, pfor="Greg Kovas", npur=1,  nres="0",ndes="1", notes="",
          des="CD: Default - Comes and Goes"),
-    dict(num=9, pfor="Erilyn Kovas", npur=0, ndes="1", notes="",
+    dict(num=9, pfor="Erilyn Kovas", npur=0,  nres="1",ndes="1", notes="",
          des="Book - Lilo and Stich"),
-    dict(num=9, pfor="Erilyn Kovas", npur=0, ndes="3", notes="",
+    dict(num=9, pfor="Erilyn Kovas", npur=0,  nres="1",ndes="3", notes="",
          des="Toy - Stuffed Bear"),
-    dict(num=10, pfor="Wesley Kovas", npur=0, ndes="2", notes="",
+    dict(num=10, pfor="Wesley Kovas", npur=0, nres="0", ndes="2", notes="",
          des="wooden blocks"),
 ]
+
 
 
 input_ = {"hd":hd, "hdr":hdr,  "wsh":wsh, "pers":pers }
