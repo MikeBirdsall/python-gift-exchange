0ff537a5f73a6f9a68bb6a9d1b7a4538927175d9
ntdragon <edwardbirdsall@elberton.net>
2021-11-26 14:34:15 -0500

    working on calendar display

--- Documentation/Design/html-examples.d/calendar.d/html_try.py
+++ Documentation/Design/html-examples.d/calendar.d/html_try.py
@@ -14,7 +14,7 @@ for i in range(0, 35,1):
      tdy.append({"bgtclr":"white","bgeclr":"white", "dnum":0, "devt":-1, "devt1t":"", "devt1c":"",  "devt2t":"", "devt2c":"",  "devt3t":"", "devt3c":"",  "devt4t":"", "devt4c":"", })
  
 
-cal = {"month":"March", "year":"2019", "startwk":5, "calAt":"Liturgical", "calBt":"US Holidays","calCt":"Birdsall Family", "calDt":"Kirkup Family", "calEt":""}
+cal = {"month":"March", "year":"2019", "startwk":5,"calrows":5, "calAt":"Liturgical", "calBt":"US Holidays","calCt":"Birdsall Family", "calDt":"Kirkup Family", "calEt":""}
 pref = {  "startDay":1,  "calAclr": "yellowgreen",  "calBclr": "lightsteelblue",  "calCclr": "cyan",  "calDclr": "magenta",  "calEclr": "purple"}
 dts = [25, 26, 27, 28, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31 ]
 
@@ -22,8 +22,11 @@ for i in range(0, 4):
           tdy[i]["bgtclr"] = colorsm["priormonth"]
           tdy[i]["bgeclr"] = colorsm["priormonth"]
 
+for i in range(35, 42):
+          tdy[i]["bgtclr"] = colorsm["nextmonth"]
+          tdy[i]["bgeclr"] = colorsm["nextmonth"]
 
-for i in range(0,35,1):
+for i in range(0,42,1):
      tdy[i]["dnum"] = dts[i]
 
 
